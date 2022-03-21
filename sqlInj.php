071287f426f93306fe192d4d94dca854a0ee9070
chenpl <810842924@qq.com>
2022-03-21 11:14:00 +0800

    0321-3

--- sqlInj.php
+++ sqlInj.php
@@ -8,7 +8,7 @@ function sqlInj($value) {
             //判断参数值中是否含有SQL关键字，如果有则跳出
             //if (stripos($value, $a) !== false) exit(json_encode(array('status' => -1, 'info' => '对不起，您提交的内容含有特殊字符：' . $a, 'data' => array($a)), 0));
             if (stripos($value, $a) !== false)
-                die("<meta http-equiv='Content-Type' content='text/html; charset=utf-8' /><script>alert('对不起，您提交的内容含有特殊字符：$a，请联系网站管理员确认')</script>");
+                die("<meta http-equiv='Content-Type' content='text/html; charset=utf-8' /><script>alert('对不起，您提交的内容含有特殊字符：$a ，请联系网站管理员确认')</script>");
         }
     } elseif (is_array($value)) {
         //如果参数值是数组则递归遍历判断
